c94201f409da242eab87a6142da657e389eb886e
StianHanssen <hanssen.stian@gmail.com>
2025-06-02 14:56:36 +0900

    feat(api): add openAPI support

---
+++ packages/api/src/handlers/docs/getOpenApiSpec.ts
@@ -0,0 +1,31 @@
+import { Logger } from '@aws-lambda-powertools/logger';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { Metrics } from '@aws-lambda-powertools/metrics';
+import { logMetrics } from '@aws-lambda-powertools/metrics/middleware';
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
+import middy from '@middy/core';
+import { generateOpenAPI } from '../../lib/openapi/generateOpenApi';
+
+const logger = new Logger();
+const tracer = new Tracer();
+const metrics = new Metrics();
+
+async function handlerLogic() {
+  logger.info('Generating OpenAPI documentation');
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(generateOpenAPI()),
+  };
+}
+
+export const handler = middy()
+  .use(captureLambdaHandler(tracer))
+  .use(injectLambdaContext(logger, { logEvent: true }))
+  .use(logMetrics(metrics, { captureColdStartMetric: true }))
+  .handler(handlerLogic);
---
+++ packages/api/src/lib/openapi/generateOpenApi.ts
@@ -0,0 +1,25 @@
+import { OpenApiGeneratorV3, OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
+import * as handlers from '../../handlers';
+
+/**
+ * Generates an OpenAPI document from the given routes.
+ * @returns The OpenAPI document
+ */
+export function generateOpenAPI(): any {
+  const registry = new OpenAPIRegistry()
+
+  for (const routeConfig of Object.values(handlers)) {
+    registry.registerPath(routeConfig);
+  }
+
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+
+  return generator.generateDocument({
+    openapi: '3.0.0',
+    info: {
+      version: '1.0.0',
+      title: 'SST API Demo',
+      description: 'This is a demo showing how to deploy an SST API with OpenAPI support',
+    },
+  });
+}
